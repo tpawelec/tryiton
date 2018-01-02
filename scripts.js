3a7003539cb270b802c2ffaa44f69632e7407e99
Tomasz Pawelec <tpawelec@outlook.com>
2018-01-02 12:31:00 +0100

    full operative ?

--- scripts.js
+++ scripts.js
@@ -1,14 +1,11 @@
 $(document).ready(function() {
 
+	// HAMBURGER MENU FOR MOBILE DEVICES
 	$('.toggle-menu').click(function() {
         $('.menu-section ul').toggleClass('menu-active');
     })
 
-	setBindings();
-
-})
-
-function setBindings() {
+	// SMOOTH SCROLL ANIMATION
 	$("#homeSection a").click(function(e) {
 		e.preventDefault();
 		var sectionId = e.currentTarget.id + "Section";
@@ -16,4 +13,26 @@ function setBindings() {
 			scrollTop: $("#" + sectionId).offset().top
 		}, 1000)
 	})
-}
+
+	// TO THE TOP ARROW
+
+	var arrow = $(".to-top");
+	$(window).scroll(function() {
+		var $this = $(this);
+
+		if($this.scrollTop() > 70) {
+			arrow.css('display', 'block');
+		} else {
+			arrow.css('display', 'none');
+		}
+	});
+
+	arrow.click(function() {
+		$("body, html").animate({
+			scrollTop: $("#homeSection").offset().top
+		}, 500)
+	})
+
+
+})
+
